97461e39383d3dd5f87e7936d698135fb7022616
Louis <louis993546@gmail.com>
2018-04-15 10:20:32 +0200

    Slight restructure of the sign in card

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,6 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Color.fromARGB(255, 249, 247, 244),
       ),
       home: new MyHomePage(title: 'Flutter Demo Home Page'),
@@ -45,6 +36,32 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool _notSignIn = true;
+
+  Widget _cardOrContent() {
+    if (_notSignIn) {
+      return new Card(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            new Text("Please sign in to your goodreads account"),
+            new Text("Email"),
+            new TextField(
+              keyboardType: TextInputType.emailAddress,
+            ),
+            new Text("Password"),
+            new TextField(
+              obscureText: true,
+            ),
+            new Text("Sign in button")
+          ],
+        ),
+        color: Color.fromARGB(255, 244, 241, 234),
+      );
+    } else {
+      return new Text("TODO")
+    }
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -73,20 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Card(
-              child: new Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new Text("Please sign in to your goodreads account"),
-                  new Text("Email"),
-                  new TextField(),
-                  new Text("Password"),
-                  new TextField(),
-                  new Text("Sign in button")
-                ],
-              ),
-              color: Color.fromARGB(255, 244, 241, 234),
-            ),
+            _cardOrContent(),
           ],
         ),
       ),
